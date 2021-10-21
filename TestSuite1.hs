376dc06519cff6bb614be64234a4c7248b6f75c1
Nursultan <nursultan_imanov@mail.ru>
2021-10-21 23:09:57 +0600

    a2

--- TestSuite1.hs
+++ TestSuite1.hs
@@ -8,20 +8,34 @@ spec :: TestTree
 spec =
   testGroup
     "Angabe1 Tests"
-    [ testCase "1" $
+    [ testCase "A1.1" $
         ist_tzr "Urknallexplosion" "knall" @?= True,
-      testCase "1" $
+      testCase "A1.2" $
         ist_tzr "Urknalexplosionkna" "knall" @?= False,
-      testCase "1" $
+      testCase "A1.3" $
         ist_tzr "aaabcd" "bcde" @?= False,
-      testCase "1" $
+      testCase "A1.4" $
         ist_tzr "aaabcdef" "abc" @?= True,
-      testCase "2" $
+      testCase "A1.5" $
         ist_tzr "Urknallexplosion" "Knall" @?= False,
-      testCase "3" $
+      testCase "A1.6" $
         ist_tzr "Urknallexplosion" "" @?= True,
-      testCase "4" $
+      testCase "A1.7" $
         ist_tzr "" "Urknallexplosion" @?= False,
-      testCase "5" $
-        ist_tzr "Urknallexplosionsknall" "knall" @?= True
+      testCase "A1.8" $
+        ist_tzr "Urknallexplosionsknall" "knall" @?= True,
+      testCase "A2.1" $
+        tzr_zeuge "Urknallexplosion" "knall" @?= ("Ur", "knall", "explosion"),
+      testCase "A2.2" $
+        tzr_zeuge "Urknallexplosion" "knall"  @?= ("Ur","knall","explosion"),
+      testCase "A2.3" $
+        tzr_zeuge "Urknallexplosion" "Knall"  @?= ("","KnallKnall",""),
+      testCase "A2.4" $
+        tzr_zeuge "Urknallexplosionsknall" "knall" @?= ("Ur","knall","explosionsknall"),
+      testCase "A2.5" $
+        tzr_zeuge "" "" @?= ("","",""),
+      testCase "A2.6" $
+        tzr_zeuge "" "asd" @?= ("","asdasd","")
+--       testCase "A2.1" $
+-- tzr_zeuge "Urknall" ""  @?= ("","","Urknall") oder ("U","","rknall") oder...
     ]
